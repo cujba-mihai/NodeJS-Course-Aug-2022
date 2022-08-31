3d6210b24e481af0c32b7b4d84cb582e757d6875
Mihai <mihai.cujba@indrivo.com>
2022-08-31 21:56:17 +0300

    Added users model

--- models/user.js
+++ models/user.js
@@ -1,16 +1,30 @@
-const Sequelize = require('sequelize');
+const { ObjectID } = require('mongodb');
+const { getDb } = require('../util/database');
 
-const sequelize = require('../util/database');
+class User {
+  constructor(username, email) {
+    this.username = username;
+    this.email = email;
+  }
 
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
+  save() {
+    const db = getDb();
+
+    return db.collection('users').insertOne(this)
+  }
+
+  addToCart(product) {
+
+  }
+
+  static findById(userId) {
+    const db = getDb();
+
+    const id = new ObjectID(userId);
+
+    return db.collection('users').findOne({ _id: id })
+  }
+
+}
 
 module.exports = User;
